19615bef348c275b32389d74630804298ccb62d8
Florencia Fernandez <florenciafernandez0301@gmail.com>
2022-05-04 23:51:26 -0300

    Incluyo captcha aleatorio y modificaciones en divs registrohtml

--- Desktop/Web1_TPE/js/script.js
+++ Desktop/Web1_TPE/js/script.js
@@ -1,4 +1,52 @@
-"use strict"
+"use strict";
+let btnCargar= document.querySelector("#nuevoCaptcha");
+let ingreso=document.querySelector("#resultadoUsuario");
+let btncheck =document.querySelector("#comprobar");
+let res= document.querySelector("#resultado");
+
+valorAletorio(); //llamo a la funcion cuando la pag se abre
+
+btnCargar.addEventListener("click", valorAletorio);
+//btncheck.addEventListener("click", validarCaptcha);
+
+
+function valorAletorio(){
+    let num =Math.floor((Math.random()*100)+1);
+    let random =document.querySelector("#aleatorio");
+    console.log(num);
+    random.innerHTML=num;
+
+
+btncheck.addEventListener("click", validarCaptcha);
+function validarCaptcha(){
+    
+ let entrada=ingreso.value;
+    console.log(entrada);
+    
+    if (entrada==num) { 
+    res.innerHTML="¡Correcto! No sos un robot.";
+    setTimeout(()=>{
+            removerResultado();
+            valorAletorio();
+        }
+     ,4000)} //cuatro segundos
+     else{
+        res.innerHTML="¡Incorrecto! Volve a intentarlo.";
+        
+        
+     }
+}  }
+
+function removerResultado (){
+    res.innerHTML="";
+    ingreso.value="";
+}
+
+
+
+
+
+/*"use strict"
 
 let btn = document.querySelector("#button");
 let suma = document.querySelector("#suma");
@@ -24,4 +72,4 @@ function captcha() {
     else {
         resultado.innerHTML = "El resultado es incorrecto. Vuelva a intentarlo";
     }
-}
+}*/
