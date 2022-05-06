1273078859438f6d711bd07c79f4291f8c6729a6
Florencia Fernandez <florenciafernandez0301@gmail.com>
2022-05-05 22:25:47 -0300

    Arreglo bug con event prevent default

--- Desktop/Web1_TPE/js/script.js
+++ Desktop/Web1_TPE/js/script.js
@@ -7,8 +7,6 @@ let res= document.querySelector("#resultado");
 valorAletorio(); //llamo a la funcion cuando la pag se abre
 
 btnCargar.addEventListener("click", valorAletorio);
-//btncheck.addEventListener("click", validarCaptcha);
-
 
 function valorAletorio(){
     let num =Math.floor((Math.random()*100)+1);
@@ -19,7 +17,7 @@ function valorAletorio(){
 
 btncheck.addEventListener("click", validarCaptcha);
 function validarCaptcha(){
-    
+   event.preventDefault(); 
  let entrada=ingreso.value;
     console.log(entrada);
     
@@ -46,30 +44,3 @@ function removerResultado (){
 
 
 
-/*"use strict"
-
-let btn = document.querySelector("#button");
-let suma = document.querySelector("#suma");
-let resultado = document.querySelector("#resultado");
-btn.addEventListener("click", captcha);
-
-let num1 = Math.floor((Math.random() * 9)+ 1);
-let num2 = Math.floor((Math.random() * 9)+ 1);
-console.log("Numeros: " +num1+ "," +num2);
-let resultadoSuma = num1 + num2;
-suma.innerHTML = num1+ "+" +num2;
-    
-let ingreso = document.querySelector("#resultadoUsuario").value;
-console.log(ingreso);
-
-
-
-function captcha() {
-    if(ingreso == resultadoSuma){
-        resultado.innerHTML = "El resultado es correcto";
-        console.log(resultado);
-    }
-    else {
-        resultado.innerHTML = "El resultado es incorrecto. Vuelva a intentarlo";
-    }
-}*/
