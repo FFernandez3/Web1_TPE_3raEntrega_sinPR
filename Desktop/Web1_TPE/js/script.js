a789415beb0abcce091743ec505079e1ad2c4af1
Florencia Fernandez <florenciafernandez0301@gmail.com>
2022-05-11 02:50:50 -0300

    Mostrar y ocultar boton enviar

--- Desktop/Web1_TPE/js/script.js
+++ Desktop/Web1_TPE/js/script.js
@@ -5,8 +5,10 @@ let btncheck = document.querySelector("#comprobar");
 let res = document.querySelector("#resultado");
 
 valorAletorio(); //llamo a la funcion cuando la pag se abre
-
+ocultarBoton(); //oculta el boton desde el inicio
+console.log("aplica ocultar boton");
 btnCargar.addEventListener("click", valorAletorio);
+
 
 function valorAletorio() {
     let num = Math.floor((Math.random() * 100) + 1);
@@ -23,14 +25,26 @@ function valorAletorio() {
 
         if (entrada == num) {
             res.innerHTML = "¡Correcto! No sos un robot.";
+            mostrarBoton();
+            /*OTRA OPCION
+            setTimeout(removerResultado, 4000); 
+             setTimeout(valorAletorio, 4000);*/
+          /*   setTimeout(() => {
+                removerResultado();
+                valorAletorio();
+            }
+                , 4000)
+                console.log("se ejecuto set time out"); */
+        } //cuatro segundos
+        else {
+            res.innerHTML = "¡Incorrecto! Volve a intentarlo.";
+            ocultarBoton();
             setTimeout(() => {
                 removerResultado();
                 valorAletorio();
             }
                 , 4000)
-        } //cuatro segundos
-        else {
-            res.innerHTML = "¡Incorrecto! Volve a intentarlo.";
+                console.log("se ejecuto set time out");
         }
     }
 }
@@ -39,7 +53,13 @@ function removerResultado() {
     res.innerHTML = "";
     ingreso.value = "";
 }
-
+function mostrarBoton(){
+    document.getElementById("enviar").classList.remove("ocultar");
+    console.log("aplica mostrar boton")
+}
+function ocultarBoton (){
+    document.getElementById("enviar").classList.add("ocultar");
+}
 
 
 
